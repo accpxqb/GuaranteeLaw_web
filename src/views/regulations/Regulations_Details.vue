7e2c586cee795bf18c6e15d4cfe784f0c3ae4c9e
刘敬 <liujing910129@outlook.com>
2024-11-14 12:19:07 +0800

    33333

---
+++ src/views/regulations/Regulations_Details.vue
@@ -0,0 +1,104 @@
+<template>
+	<el-row>
+		<el-col :span="24">
+			<!--? Hero Start -->
+			<div class="slider-area2">
+				<div class="slider-height2 hero-overly2 d-flex align-items-center">
+					<div class="container">
+						<div class="row">
+							<div class="col-xl-12">
+								<div class="hero-cap hero-cap2 text-center">
+									<h2>栏目名称-文章详情</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Hero End -->
+		</el-col>
+		<el-col :span="24">
+			<!--================Blog Area =================-->
+			<section class="blog_area single-post-area section-padding1">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 posts-list">
+							<div class="single-post">
+
+								<div class="blog_details">
+									<h2>法律名称-文章标题
+									</h2>
+									<ul class="blog-info-link mt-3 mb-4">
+										<li><i class="fa fa-users"></i>作者</li>
+										<li><i class="fa fa-comments"></i>类型</li>
+										<li><i class="fa fa-calendar-times"></i>2024-11-01</li>
+									</ul>
+									<div class="quote-wrapper">
+										<div class="quotes">
+											<p>文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介文章简介
+											</p>
+										</div>
+									</div>
+									<div class="feature-img">
+										<img class="img-fluid" src="assets/img/blog/single_blog_1.png" alt=""
+											width="100%">
+									</div>
+									<p>
+										文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容
+										文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容
+										文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容文章内容
+									</p>
+								</div>
+							</div>
+							<div class="navigation-top">
+								<div class="navigation-area">
+									<div class="row">
+										<div
+											class="col-lg-6 col-md-6 col-12 nav-left flex-row d-flex justify-content-start align-items-center">
+											<div class="thumb">
+												<a href="#">
+													<img class="img-fluid" src="assets/img/post/preview.png" alt="">
+												</a>
+											</div>
+											<div class="arrow">
+												<a href="#">
+													<span class="lnr text-white ti-arrow-left"></span>
+												</a>
+											</div>
+											<div class="detials">
+												<p>上一篇</p>
+												<a href="#">
+													<h4>文章标题文章标题文章标题</h4>
+												</a>
+											</div>
+										</div>
+										<div
+											class="col-lg-6 col-md-6 col-12 nav-right flex-row d-flex justify-content-end align-items-center">
+											<div class="detials">
+												<p>下一篇</p>
+												<a href="#">
+													<h4>文章标题文章标题文章标题</h4>
+												</a>
+											</div>
+											<div class="arrow">
+												<a href="#">
+													<span class="lnr text-white ti-arrow-right"></span>
+												</a>
+											</div>
+											<div class="thumb">
+												<a href="#">
+													<img class="img-fluid" src="assets/img/post/next.png" alt="">
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!--================ Blog Area end =================-->
+		</el-col>
+	</el-row>
+</template>
